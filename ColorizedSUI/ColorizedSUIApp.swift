71d034235ed1ee23798df29e4d7040b38b2a6997
denkitdam <cryptoden145@gmail.com>
2023-06-12 19:16:26 +0300

    Initial Commit

---
+++ ColorizedSUI/ColorizedSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSUIApp.swift
+//  ColorizedSUI
+//
+//  Created by Denis Kitaev on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
